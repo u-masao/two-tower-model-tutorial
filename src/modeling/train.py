d3aeb31674090408e60ed77c0aaf59ae7ff9987d
u-masao <4973920+u-masao@users.noreply.github.com>
2024-10-11 03:49:12 +0900

    add starter code

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,30 +1,91 @@
-from pathlib import Path
-
-import typer
+import click
+import cloudpickle
+import mlflow
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from loguru import logger
-from tqdm import tqdm
+from torch.utils.data import DataLoader, Dataset
 
-from src.config import MODELS_DIR, PROCESSED_DATA_DIR
+from src import config  # noqa: F401
+from src.modeling.model import TwoTowerModel
 
-app = typer.Typer()
+
+def train(dataloader, num_epochs=100):
+    # モデル、オプティマイザ、損失関数の定義
+    model = TwoTowerModel(user_embed_dim=128, item_embed_dim=64)
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.CosineEmbeddingLoss()
+
+    # 学習ループ
+    for epoch in range(num_epochs):
+        for user_embeds, item_embeds, labels in dataloader:
+            optimizer.zero_grad()
+            user_repr, item_repr = model(user_embeds, item_embeds)
+            loss = criterion(user_repr, item_repr, labels)
+            loss.backward()
+            optimizer.step()
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+def load_dataset(input_filepath):
+    return cloudpickle.load(open(input_filepath, "rb"))["train"]
+
+
+class TripletDataset(Dataset):
+    def __init__(self, user_embeds, item_embeds, ratings, rating_threshold=5):
+        self.user_embeds = user_embeds
+        self.item_embeds = item_embeds
+        self.ratings = ratings
+        self.rating_threshold = rating_threshold
+
+    def __len__(self):
+        return len(self.ratings)
+
+    def __getitem__(self, idx):
+        targets = self.ratings[idx]
+        users = self.user_embeds.loc[targets["user-id"]].values
+        items = self.item_embeds.loc[targets["isbn"]].values
+        ratings = (targets["rating"] > self.threshold).astype(float).values
+        return users, items, ratings
+
+
+def make_dataloader(dataset):
+    return DataLoader(dataset, batch_size=32, shuffle=True)
+
+
+@click.command()
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
+@click.option("--mlflow_run_name", type=str, default="develop")
+def main(**kwargs):
+
+    # init log
+    logger.info("==== start process ====")
+    mlflow.set_experiment("build features")
+    mlflow.start_run(run_name=kwargs["mlflow_run_name"])
+
+    # log cli args
+    logger.info(f"cli args: {kwargs}")
+    mlflow.log_params({f"args.{k}": v for k, v in kwargs.items()})
+
+    # load dataset
+    dataset = load_dataset(kwargs["input_filepath"])
+
+    # make dataloader
+    dataloader = make_dataloader(dataset)
+
+    # build features
+    model = train(dataloader)
+
+    # output file
+    torch.save(model.state_dict(), kwargs["output_filepath"])
+
+    # logging
+
+    # cleanup
+    mlflow.end_run()
+    logger.success("==== complete process ====")
 
 
 if __name__ == "__main__":
-    app()
+    main()
