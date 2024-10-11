ba76991fcbfbbe236f9d4024a19e0d44378599b3
u-masao <4973920+u-masao@users.noreply.github.com>
2024-10-12 01:50:18 +0900

    add score hist

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import click
+import matplotlib.pyplot as plt
 import mlflow
 import pandas as pd
 import torch
@@ -17,22 +20,38 @@ def predict(model, dataloader):
     for user_embeds, item_embeds, labels in tqdm(dataloader["test"]):
         user_repr, item_repr = model(user_embeds, item_embeds)
         cosine = F.cosine_similarity(user_repr, item_repr, dim=1, eps=1e-8)
-        # predict = (cosine > 0.5).to(int)
         result.append(cosine)
-    return torch.cat(result)
+    return pd.DataFrame(torch.cat(result).tolist())
 
 
 def load_model(
-    model_filepath, user_embed_dim=384, item_embed_dim=384, hidden_dim=384
+    model_filepath: str,
+    user_embed_dim: int = 384,
+    item_embed_dim: int = 384,
+    hidden_dim: int = 384,
+    output_dim: int = 384,
 ):
     model = TwoTowerModel(
         user_embed_dim=user_embed_dim,
         item_embed_dim=item_embed_dim,
         hidden_dim=hidden_dim,
+        output_dim=output_dim,
     )
     model.load_state_dict(torch.load(model_filepath, weights_only=True))
     model.eval()
     return model
+
+
+def analysis(df, output_dir):
+    output_dir_path = Path(output_dir)
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    fig, ax = plt.subplots()
+    df.hist(ax=ax, bins=101)
+    ax.set_yscale("log")
+    fig.savefig(output_dir_path / "pred_scores_histogram.png")
+    fig.clf()
+    plt.close(fig)
 
 
 @click.command()
@@ -41,6 +60,9 @@ def load_model(
 @click.argument("output_filepath", type=click.Path())
 @click.option("--mlflow_run_name", type=str, default="develop")
 @click.option("--batch_size", type=int, default=32)
+@click.option(
+    "--output_chart_dir", type=click.Path(), default="reports/figures/predict/"
+)
 def main(**kwargs):
 
     # init log
@@ -64,10 +86,11 @@ def main(**kwargs):
     predicted = predict(model, dataloader)
 
     # output file
-    pd.DataFrame(predicted.tolist()).to_parquet(kwargs["output_filepath"])
+    predicted.to_parquet(kwargs["output_filepath"])
 
     # logging
-    logger.info(predicted.tolist())
+    logger.info(predicted)
+    analysis(predicted, kwargs["output_chart_dir"])
 
     # cleanup
     mlflow.end_run()
