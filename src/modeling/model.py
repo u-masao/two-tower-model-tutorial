04af97bfe91e8ecb00b0eed03cd5a7a9c1ec584e
u-masao <4973920+u-masao@users.noreply.github.com>
2024-10-12 01:49:58 +0900

    add dropout

--- src/modeling/model.py
+++ src/modeling/model.py
@@ -2,17 +2,26 @@ import torch.nn as nn
 
 
 class TwoTowerModel(nn.Module):
-    def __init__(self, user_embed_dim, item_embed_dim, hidden_dim=64):
+    def __init__(
+        self,
+        user_embed_dim,
+        item_embed_dim,
+        hidden_dim=64,
+        output_dim=64,
+        dropout_p=0.5,
+    ):
         super(TwoTowerModel, self).__init__()
         self.user_tower = nn.Sequential(
             nn.Linear(user_embed_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(hidden_dim, output_dim),
         )
         self.item_tower = nn.Sequential(
             nn.Linear(item_embed_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(hidden_dim, output_dim),
         )
 
     def forward(self, user_embed, item_embed):
