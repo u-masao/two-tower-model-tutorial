44f6c461ac6fd7ba8719d30dc1a72578b64a40a6
u-masao <4973920+u-masao@users.noreply.github.com>
2024-10-12 23:23:26 +0900

    add calc test loss

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -6,8 +6,10 @@ import mlflow
 import mlflow.pytorch
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from loguru import logger
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from src import config  # noqa: F401
@@ -16,24 +18,17 @@ from src.modeling.model import TwoTowerModel
 
 
 def train(
-    dataloader,
-    output_model_dir,
-    num_epochs=3,
-    log_interval=500,
-    model_save_interval_epochs=10,
+    dataloader: DataLoader,
+    output_model_dir: str,
+    num_epochs: int = 3,
+    log_interval: int = 500,
+    model_save_interval_epochs: int = 10,
+    proba_threshold: float = 0.5,
 ):
+
+    # make output dir
     output_model_dir = Path(output_model_dir)
     output_model_dir.mkdir(parents=True, exist_ok=True)
-
-    logger.info(dataloader)
-    for u, i, l in dataloader["train"]:
-        logger.info(f"{u=}")
-        logger.info(f"{i=}")
-        logger.info(f"{l=}")
-        logger.info(f"{u.shape=}")
-        logger.info(f"{i.shape=}")
-        logger.info(f"{l.shape=}")
-        break
 
     # モデル、オプティマイザ、損失関数の定義
     model = TwoTowerModel(
@@ -42,21 +37,23 @@ def train(
     optimizer = optim.Adam(model.parameters())
     criterion = nn.CosineEmbeddingLoss()
 
-    # 学習ループ
+    # train evaluate loop
     for epoch in range(num_epochs):
+        # train
+        model.train()
         with tqdm(dataloader["train"]) as pbar:
             for batch_idx, (user_embeds, item_embeds, labels) in enumerate(
                 pbar
             ):
                 optimizer.zero_grad()
                 user_repr, item_repr = model(user_embeds, item_embeds)
-                loss = criterion(user_repr, item_repr, labels)
-                loss.backward()
+                train_loss = criterion(user_repr, item_repr, labels)
+                train_loss.backward()
                 optimizer.step()
                 if batch_idx % log_interval == 0:
                     pbar.set_description(f"epoch={epoch}")
-                    pbar.set_postfix(OrderedDict(loss=loss.item()))
-                    metrics = {"loss": loss.item()}
+                    pbar.set_postfix(OrderedDict(train_loss=train_loss.item()))
+                    metrics = {"train.loss": train_loss.item()}
                     step = len(pbar) * epoch + batch_idx
                     mlflow.log_metrics(metrics, step=step)
 
@@ -65,6 +62,39 @@ def train(
                 model.state_dict(),
                 output_model_dir / f"model_epoch{epoch:04}.pth",
             )
+
+        # evaluate
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            with tqdm(dataloader["test"]) as pbar:
+                for batch_idx, (user_embeds, item_embeds, labels) in enumerate(
+                    pbar
+                ):
+                    user_repr, item_repr = model(user_embeds, item_embeds)
+                    test_loss += criterion(user_repr, item_repr, labels)
+                    cosine = F.cosine_similarity(
+                        user_repr, item_repr, dim=1, eps=1e-8
+                    )
+                    pred = (
+                        ((cosine > proba_threshold).to(float) - 0.5) * 2
+                    ).to(int)
+                    correct += pred.eq(labels).sum().item()
+
+        test_data_size = len(dataloader["test"].dataset)
+        test_loss /= test_data_size
+        test_metrics = {
+            "test.loss": test_loss,
+            "test.data_size": test_data_size,
+            "test.correct": correct,
+            "test.accuracy": correct / test_data_size,
+        }
+        logger.info(f"{test_metrics=}")
+        mlflow.log_metrics(
+            test_metrics,
+            step=epoch,
+        )
 
     return model
 
@@ -86,9 +116,6 @@ def main(**kwargs):
     # log cli args
     logger.info(f"cli args: {kwargs}")
     mlflow.log_params({f"args.{k}": v for k, v in kwargs.items()})
-
-    # autolog
-    mlflow.pytorch.autolog()
 
     # make dataloader
     dataloader = make_dataloader(
